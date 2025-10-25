cdbbdab406c95fe65ca3144806341b50ddaed860
luisalois131-design <luisalois131@gmail.com>
2025-10-25 03:15:03 +0300

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "20px", marginTop: "40px", background: "#1E90FF", color: "#fff" }}>
+      <p>© 2025 NFT Ticket DApp</p>
+    </footer>
+  );
+}
