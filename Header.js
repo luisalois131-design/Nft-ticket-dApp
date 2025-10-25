d5b9b179ac2c3a4a8c58c457fade24a7706ba745
luisalois131-design <luisalois131@gmail.com>
2025-10-25 03:14:27 +0300

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ textAlign: "center", padding: "20px", background: "#1E90FF", color: "#fff" }}>
+      <h1>NFT Ticket DApp</h1>
+      <p>Mint and manage your tickets securely on Hedera</p>
+    </header>
+  );
+}
